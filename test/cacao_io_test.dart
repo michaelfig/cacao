eaf63ff690e9e454969fc9e3bb71910fe1aaab20
Michael FIG <michael@liveblockauctions.com>
2018-12-06 21:24:31 -0600

    Strengthen cacao_io_test.

--- test/cacao_io_test.dart
+++ test/cacao_io_test.dart
@@ -5,7 +5,8 @@ void main() {
   test('constants', () {
     expect(DEFAULT_HOST, '127.0.0.1');
     expect(DEFAULT_PORT, 8997);
-    expect(DEFAULT_SCHEME_MAP[null], (srv) => srv != null);
-    expect(DEFAULT_SCHEME_MAP['file'], (srv) => srv != null);
+    expect(DEFAULT_SCHEME_MAP[null], new TypeMatcher<CacaoServer>());
+    expect(DEFAULT_SCHEME_MAP['file'], new TypeMatcher<CacaoServer>());
+    expect(DEFAULT_SCHEME_MAP['unknown'], isNull);
   });
 }
