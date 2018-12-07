69c1219fb03b0e3335a814271f5caef52ad17092
Stewart Berntson <mydnight@technomagia.tk>
2018-12-07 10:13:49 -0600

    Straighten out async streams.

--- lib/src/io/serveFile.dart
+++ lib/src/io/serveFile.dart
@@ -1,4 +1,5 @@
 import "dart:io";
+import "dart:async";
 import "package:mime/mime.dart";
 
 Future<void> serveFile(HttpRequest request, Uri uri) async {
@@ -10,25 +11,15 @@ Future<void> serveFile(HttpRequest request, Uri uri) async {
   var file = new File(path).openRead();
 
   bool sentHeaders = false;
-  final subscription = file.listen(
-          //Check MIME type and make sure we only send the HTTP headers once
-          (event) {
-              if (!sentHeaders){
-                sentHeaders = true;
-                final contentTypeList = lookupMimeType(path,headerBytes:event).split('/');
-                //final contentType = new ContentType(contentTypeList.split('/')[1],);
-                request.response.headers.contentType = new ContentType(contentTypeList[0],contentTypeList[1]);
-                request.response.headers.set('Access-Control-Allow-Origin', '*');
-              }
-              request.response.add(event);
-          },
-          //We have all the data we need for the response
-          onDone: () => request.response.close(),
-          //Bubble up to serve.dart error handler
-          onError: (e) => request.response.addError(e)
-        );
-        //Cancel the subscription after we are done
-        await request.response.done.then((_) {
-          subscription.cancel();
-        });
+  await for (final event in file) {
+    if (!sentHeaders){
+      sentHeaders = true;
+      final contentTypeList = lookupMimeType(path,headerBytes:event).split('/');
+      //final contentType = new ContentType(contentTypeList.split('/')[1],);
+      request.response.headers.contentType = new ContentType(contentTypeList[0],contentTypeList[1]);
+      request.response.headers.set('Access-Control-Allow-Origin', '*');
+    }
+    request.response.add(event);
+  }
+  await request.response.close();
 }
